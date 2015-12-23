e202b0beac6aa2d5008afc3681339b36be4965f1
Oscar G.M <oscar@wustl.edu>
2015-12-22 21:35:43 -0600

    split my bill in progress

---
+++ tips/SplitView.swift
@@ -0,0 +1,13 @@
+//
+//  SplitView.swift
+//  tips
+//
+//  Created by Oscar G.M on 12/22/15.
+//  Copyright © 2015 GMStudios. All rights reserved.
+//
+
+import Cocoa
+
+class SplitView: UIView {
+
+}
